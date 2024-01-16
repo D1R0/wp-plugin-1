d0239eb661bcf5718247806ba297467ce77804a5
cava <adrian.valentindr@gmail.com>
2024-01-16 11:05:28 +0200

    fixed

--- edit-project-form.php
+++ edit-project-form.php
@@ -2,20 +2,27 @@
 
 function ch4_plugin_activate()
 {
+    updateDatabase();
+}
+
+function updateDatabase()
+{
     global $wpdb;
 
     $table_name = $wpdb->prefix . 'ch4_projects';
     $charset_collate = $wpdb->get_charset_collate();
+
     if ($wpdb->get_var("show tables like '$table_name'") != $table_name) {
         $sql = "CREATE TABLE $table_name (
-        series VARCHAR(255) NOT NULL,
-        name VARCHAR(255) NOT NULL,
-        status VARCHAR(255),
-        details LONGTEXT,
-        PRIMARY KEY (series)
-    ) $charset_collate;";
+            series VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            status VARCHAR(255),
+            details LONGTEXT,
+            PRIMARY KEY (series)
+        ) $charset_collate;";
+
+        // Execute the SQL query
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
     }
-    $result = $wpdb->get_results($sql);
-    // require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    // dbDelta($sql);
 }
